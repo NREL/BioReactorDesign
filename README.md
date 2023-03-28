c22215d6984bc75d2b5b83ffe47094e27073b900
Malik <malik.hassanaly@gmail.com>
2023-03-27 20:37:58 -0600

    adding inhomogeneous BC utility

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Generate STL of spider sparger
+## Generate STL of spider sparger
 
-## Execute without plotting
+### Execute without plotting
 
 `python main.py -cr 0.25 -na 12 -aw 0.1 -al 0.5`
 
-## Execute with plotting
+### Execute with plotting
 
 `python main.py -v -cr 0.25 -na 12 -aw 0.1 -al 0.5`
 
@@ -15,7 +15,7 @@ Generates
 </p>
 
 
-## Manual
+### Manual
 
 ```
 usage: main.py [-h] [-cr] [-na] [-aw] [-al] [-v]
@@ -30,6 +30,39 @@ optional arguments:
   -aw , --armsWidth     Width of spider arms
   -al , --armsLength    Length of spider arms
   -v, --verbose         plot on screen
+
+```
+
+## Generate fi.gas
+
+`cd inhomogeneousBC`
+
+### Execute
+
+Generates `fi.gas` in `IC_inhomo`. If `r<0.1` use pores of diameter `3e-5`. Gradually decrease the pore size to `2e-5` linearly.
+
+`python main.py -rc 0.1 -re 1 -pi 3e-5 -po 2e-5 -xc 0 -zc 0`
+
+### Execute with logging
+
+`python main.py -rc 0.1 -re 1 -pi 3e-5 -po 2e-5 -xc 0 -zc 0 -v`
+
+### Manual
+
+```
+usage: main.py [-h] [-v] [-rc] [-re] [-pi] [-po] [-xc] [-zc]
+
+Generate inhomogeneous boundary
+
+optional arguments:
+  -h, --help         show this help message and exit
+  -v, --verbose      plot on screen
+  -rc , --r_const    Constant radius value
+  -re , --r_end      End radius value
+  -pi , --pore_in    Pore diameter at center
+  -po , --pore_out   Pore diameter at radius end
+  -xc , --xcent      Column center x
+  -zc , --zcent      Column center z
 
 ```
 
---
+++ util/argument_inhomo.py
@@ -0,0 +1,69 @@
+import argparse
+
+def initArgs():
+    # CLI
+    parser = argparse.ArgumentParser(description="Generate inhomogeneous boundary")
+    parser.add_argument(
+        "-v", 
+        "--verbose",
+        action="store_true",
+        help="plot on screen"
+    )
+    parser.add_argument(
+        "-rc",
+        "--r_const",
+        type=float,
+        metavar="",
+        required=False,
+        help="Constant radius value",
+        default=0.1,
+    )
+    parser.add_argument(
+        "-re",
+        "--r_end",
+        type=float,
+        metavar="",
+        required=False,
+        help="End radius value",
+        default=1,
+    )
+    parser.add_argument(
+        "-pi",
+        "--pore_in",
+        type=float,
+        metavar="",
+        required=False,
+        help="Pore diameter at center",
+        default=3e-5,
+    )
+    parser.add_argument(
+        "-po",
+        "--pore_out",
+        type=float,
+        metavar="",
+        required=False,
+        help="Pore diameter at radius end",
+        default=2e-5,
+    )
+    parser.add_argument(
+        "-xc",
+        "--xcent",
+        type=float,
+        metavar="",
+        required=False,
+        help="Column center x",
+        default=0.0,
+    )
+    parser.add_argument(
+        "-zc",
+        "--zcent",
+        type=float,
+        metavar="",
+        required=False,
+        help="Column center z",
+        default=0.0,
+    )
+
+    args = parser.parse_args()
+
+    return args
