98d75e2b36b5da44088ee222ce7ec7210aad962d
Malik <malik.hassanaly@gmail.com>
2023-07-10 13:39:41 -0600

    pass the diffusion argument to python

--- computeQoI/exec_comp_final.sh
+++ computeQoI/exec_comp_final.sh
@@ -5,17 +5,17 @@ diff_val="1.2097e-8 1.4663e-9 3.9518e-9"
 
 computeQOI () {
     for dir in $1/*/; do
-        python compute_QOI.py -f $dir -avg 5 -conv 100 -vl $fields
+        python compute_QOI.py -f $dir -avg 5 -conv 100 -vl $fields -dn $diff_name -dv $diff_val
     done
 }
 
 rootFolder=/projects/disrupt/mhassana/sparger_geom_coarse_june21/
-computeQOI $rootFolder/study_coarse_flatDonut -dn $diff_name -dv $diff_val
-computeQOI $rootFolder/study_coarse_multiRing -dn $diff_name -dv $diff_val
-computeQOI $rootFolder/study_coarse_sideSparger -dn $diff_name -dv $diff_val
+computeQOI $rootFolder/study_coarse_flatDonut
+computeQOI $rootFolder/study_coarse_multiRing
+computeQOI $rootFolder/study_coarse_sideSparger
 
 rootFolder=/projects/disrupt/mhassana/sparger_geom_fine_june21/
-computeQOI $rootFolder/study_fine_flatDonut -dn $diff_name -dv $diff_val
-computeQOI $rootFolder/study_fine_multiRing -dn $diff_name -dv $diff_val
-computeQOI $rootFolder/study_fine_multiRing_num -dn $diff_name -dv $diff_val
-computeQOI $rootFolder/study_fine_sideSparger -dn $diff_name -dv $diff_val
+computeQOI $rootFolder/study_fine_flatDonut
+#computeQOI $rootFolder/study_fine_multiRing
+#computeQOI $rootFolder/study_fine_multiRing_num
+#computeQOI $rootFolder/study_fine_sideSparger
