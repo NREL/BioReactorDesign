af2a1f7eebb66a8599f5772d660b68c700321986
Malik <malik.hassanaly@gmail.com>
2023-07-10 13:52:53 -0600

    differentiate between klas

--- process_geom/exec_plot.sh
+++ process_geom/exec_plot.sh
@@ -1,4 +1,4 @@
 fields="GH GH_height d CO2_liq CO_liq H2_liq kla_CO2 kla_CO kla_H2"
-rootFolder=../sparger_geom/study_coarse_flatDonut/
+rootFolder=../../spargerDesign_june16/sparger_geom/study_coarse_flatDonut/
 
 python plot_qoi.py -sf $rootFolder -cp flat_donut -vl $fields -pl width -ff flat_donut -p param_flatDonut.npz -cfe flat_donut_4 
