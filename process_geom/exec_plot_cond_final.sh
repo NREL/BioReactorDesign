89276e8b7ccb4eab969bb77073ecf4ccc6a2b91c
Malik <malik.hassanaly@gmail.com>
2023-07-10 13:57:13 -0600

    local plot

--- process_geom/exec_plot_cond_final.sh
+++ process_geom/exec_plot_cond_final.sh
@@ -4,16 +4,15 @@ plotCond () {
     python plot_conditionalMean_geom.py -sf $1 -cp $2 -fl $fields -pl $3 -ff $4 -p $5
 }
 
-rootFolder=/projects/disrupt/mhassana/sparger_geom_coarse_june21/
+rootFolder=/Users/mhassana/Desktop/GitHub/spargerDesign_june25/caseResults_geom/coarse
 plotCond $rootFolder/study_coarse_flatDonut flat_donut width flat_donut_coarse param_flatDonut.npz
 plotCond $rootFolder/study_coarse_multiRing multiRing width multiRing_coarse param_multiRing.npz
 plotCond $rootFolder/study_coarse_multiRing multiRing spacing multiRing_coarse param_multiRing.npz
-plotCond $rootFolder/study_coarse_sideSparger side_sparger height side_sparger_coarse param_sideSparger.npz
+plotQOI $rootFolder/study_coarse_sideSparger side_sparger height side_sparger_coarse param_sideSparger.npz
 
-
-rootFolder=/projects/disrupt/mhassana/sparger_geom_fine_june21/
+rootFolder=/Users/mhassana/Desktop/GitHub/spargerDesign_june25/caseResults_geom/fine
 plotCond $rootFolder/study_fine_flatDonut flat_donut width flat_donut_fine param_flatDonut.npz
-plotCond $rootFolder/study_fine_multiRing multiRing width multiRing_fine param_multiRing.npz
-plotCond $rootFolder/study_fine_multiRing multiRing spacing multiRing_fine param_multiRing.npz
-plotCond $rootFolder/study_fine_sideSparger side_sparger height side_sparger_fine param_sideSparger.npz
+#plotCond $rootFolder/study_fine_multiRing multiRing width multiRing_fine param_multiRing.npz
+#plotCond $rootFolder/study_fine_multiRing multiRing spacing multiRing_fine param_multiRing.npz
+#plotCond $rootFolder/study_fine_sideSparger side_sparger height side_sparger_fine param_sideSparger.npz
 
