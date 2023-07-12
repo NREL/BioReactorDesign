c365bec85c681bbc4ac60847a1ea8e0897b3c2e7
Malik <malik.hassanaly@gmail.com>
2023-07-12 17:20:26 -0600

    add validation plot with paper quality

---
+++ validation/exec_comp.sh
@@ -0,0 +1,6 @@
+python compareToExp.py  -rd refData/data/val2_data.npz \
+-f19 case19 \
+-f17 case17 \
+-ff val
+
+
---
+++ validation/util/folderManagement.py
@@ -0,0 +1,10 @@
+import os
+
+def makeRecursiveFolder(path):
+    folder_list = path.split("/")
+    localFolder = ""
+    for folder in folder_list:
+        localFolder = os.path.join(localFolder,folder)
+        os.makedirs(localFolder,exist_ok=True)
+
+
