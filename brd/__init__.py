5b78d6aebf33eb5bc66c4aea11b8d92c2a544c15
Malik <malik.hassanaly@gmail.com>
2024-01-31 10:05:20 -0700

    modularize brd

---
+++ brd/__init__.py
@@ -0,0 +1,7 @@
+"""Bio reactor design"""
+import os
+from brd.version import __version__
+
+BRD_DIR = os.path.dirname(os.path.realpath(__file__))
+BRD_MESH_DIR = os.path.join(BRD_DIR, "meshing")
+BRD_INV_DIR = os.path.join(UIPS_DIR, "inverse_modeling")
---
+++ brd/version.py
@@ -0,0 +1,3 @@
+"""Bio reactor design version"""
+
+__version__ = "0.0.1"
