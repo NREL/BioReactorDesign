314d89ea30f0eca604b81ac2ae9d0af26c0ee6bc
Malik <malik.hassanaly@gmail.com>
2024-12-15 19:24:53 -0700

    format

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,21 +1,24 @@
 # Configuration file for the Sphinx documentation builder.
 import os
 import sys
+
 # Need this so sphinx can find lumache.py. Change is .py files are elsewhere than root.
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../bird'))
+sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../bird"))
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, '..', '..',  "bird", "version.py"), encoding="utf-8") as f:
+with open(
+    os.path.join(here, "..", "..", "bird", "version.py"), encoding="utf-8"
+) as f:
     version = f.read()
 version = version.split("=")[-1].strip().strip('"').strip("'")
 
 # -- Project information
 
 project = "Bio Reactor Design (BiRD)"
-copyright = '2024'
-author = 'NREL'
+copyright = "2024"
+author = "NREL"
 
 release = version
 version = version
@@ -23,44 +26,44 @@ version = version
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-#    'sphinxcontrib.bibtex',
-#    'autoapi.extension',
-#    'sphinxcontrib.apidoc',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    #    'sphinxcontrib.bibtex',
+    #    'autoapi.extension',
+    #    'sphinxcontrib.apidoc',
 ]
-#bibtex_bibfiles = ["references.bib"]
-#autoapi_type = 'python'
-#autoapi_dirs = ['../../bird']
+# bibtex_bibfiles = ["references.bib"]
+# autoapi_type = 'python'
+# autoapi_dirs = ['../../bird']
 
-#apidoc_module_dir = '../../src'
-#apidoc_output_dir = '.'
-#apidoc_excluded_paths = ['tests']
-#apidoc_separate_modules = True
+# apidoc_module_dir = '../../src'
+# apidoc_output_dir = '.'
+# apidoc_excluded_paths = ['tests']
+# apidoc_separate_modules = True
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ["std"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # -- Options for EPUB output
-epub_show_urls = 'footnote'
+epub_show_urls = "footnote"
 
 html_context = {
-    "display_github": True, # Integrate GitHub
-    "github_repo": "NREL/BioReactorDesign", # Repo name
-    "github_version": "main", # Version
-    "conf_py_path": "docs/source/", # Path in the checkout to the docs root
+    "display_github": True,  # Integrate GitHub
+    "github_repo": "NREL/BioReactorDesign",  # Repo name
+    "github_version": "main",  # Version
+    "conf_py_path": "docs/source/",  # Path in the checkout to the docs root
 }
 
 # -- Options for HTML output -------------------------------------------------
@@ -73,7 +76,7 @@ html_show_copyright = True
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 repository_url = f"https://github.com/NREL/BioReactorDesign"
 html_context = {
     "menu_links": [
