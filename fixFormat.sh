987d8c46512664135ec4143f2d1ddb6d75c831ae
Malik <malik.hassanaly@gmail.com>
2025-01-16 16:25:26 -0700

    cleanup format fixing script

--- fixFormat.sh
+++ fixFormat.sh
@@ -1,10 +1,4 @@
 source .github/linters/formatting.sh
 
 format .
-#blockCyclindricalMeshing
-#format inhomogeneousBC
-#format spiderSparger 
-#format conditionalMean
-#format computeQoI
-#format process_geom
 
