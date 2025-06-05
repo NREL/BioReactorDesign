fix concentration computation and default cstar

tutorial_cases/loop_reactor_mixing/read_history.py
@@ -97,11 +97,15 @@ def cliq(caseFolder, timeFolder, nCells, field_dict={}):
     # c_h2 = rho_liq[indliq] * alpha_liq[indliq] * h2_liq[indliq] / 0.002016
     # c_co2 = rho_liq[indliq] * alpha_liq[indliq] * co2_liq[indliq] / 0.04401
 
-    c_h2 = 1000 * alpha_liq[indliq] * h2_liq[indliq] / 0.002016
-    c_co2 = 1000 * alpha_liq[indliq] * co2_liq[indliq] / 0.04401
+    c_h2 = 1000 * h2_liq[indliq] / 0.002016
+    c_co2 = 1000 * co2_liq[indliq] / 0.04401
 
-    c_h2 = np.sum(c_h2 * volume[indliq]) / np.sum(volume[indliq])
-    c_co2 = np.sum(c_co2 * volume[indliq]) / np.sum(volume[indliq])
+    c_h2 = np.sum(c_h2 * volume[indliq] * alpha_liq[indliq]) / np.sum(
+        volume[indliq] * alpha_liq[indliq]
+    )
+    c_co2 = np.sum(c_co2 * volume[indliq] * alpha_liq[indliq]) / np.sum(
+        volume[indliq] * alpha_liq[indliq]
+    )
 
     return c_co2, c_h2, field_dict
 

tutorial_cases/loop_reactor_pbe_dynmix_nonstat_headbranch_scaleup/get_qoi.py
@@ -139,8 +139,8 @@ dataFolder = "data"
 fold = "local"
 
 nuq = 100
-mean_cstar_co2 = np.random.uniform(12.6, 13.3, nuq)
-mean_cstar_h2 = np.random.uniform(0.902, 0.96, nuq)
+mean_cstar_co2 = np.random.uniform(14, 16.9, nuq)
+mean_cstar_h2 = np.random.uniform(1.04, 1.19, nuq)
 
 
 tmp_cs_h2 = []