690e0f7f1ef30c589cbe0a3c5944d9bb4ec9d4f8
Malik <malik.hassanaly@gmail.com>
2025-09-02 13:03:05 -0600

    remove unused import and add comments in relation to the doc

--- applications/compute_time_series_mean.py
+++ applications/compute_time_series_mean.py
@@ -1,7 +1,6 @@
 import logging
 
 import numpy as np
-from prettyPlot.plotting import plt, pretty_labels
 
 from bird.postprocess.stats import calc_mean
 
@@ -10,12 +9,14 @@ logger = logging.getLogger(__name__)
 
 def main():
 
+    # Sample with 100 points
     t = np.linspace(0, 2, 100)
     signal = np.cos(t * 2 * np.pi)
 
     mean_val, unc_val = calc_mean(signal, t)
     logger.info(f"Mean = {mean_val:.2g} +/- {unc_val:.2g}")
 
+    # Oversample with 10000 points
     t = np.linspace(0, 2, 10000)
     signal = np.cos(t * 2 * np.pi)
     mean_val, unc_val = calc_mean(signal, t)
