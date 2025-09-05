3b68b30c9617d1d3b2bbae462c56470eecdb348a
Malik <malik.hassanaly@gmail.com>
2025-09-04 18:21:24 -0600

    safety check for T0

--- bird/postprocess/stats.py
+++ bird/postprocess/stats.py
@@ -78,6 +78,13 @@ def _T0_fn(autocorr: np.ndarray) -> float:
     k = np.array(range(1, N))
     T0 = 1.0 + 2.0 * np.sum((1.0 - k / N) * autocorr[1:])
 
+    # Check value of T0
+    if T0 < 1.0 or T0 > N / 2:
+        logger.warning(
+            f"T0 value ({T0}) is suspicious, falling back to T0 = 1"
+        )
+        T0 = 1.0
+
     logger.debug(f"T0 = {T0}")
 
     return T0
