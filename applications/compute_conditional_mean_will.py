cfc3fbc991a864eb0ba7b30e1196e71d07a6ea32
Malik <malik.hassanaly@gmail.com>
2025-09-11 17:21:11 -0600

    demo radial mean for will

---
+++ applications/compute_conditional_mean_will.py
@@ -0,0 +1,327 @@
+from bird.postprocess.post_quantities import *
+from bird.utilities.mathtools import *
+from bird.utilities.ofio import *
+
+logger = logging.getLogger(__name__)
+
+from pathlib import Path
+
+
+def height2str(height: float) -> str:
+    """
+    Convert height value to a str
+    """
+    return f"{height:.2g}"
+
+
+def compute_radius_field(cellCentres: np.ndarray, vert_ind: int) -> np.ndarray:
+    """
+    Compute radius from cell centres. The radius goes negative to make it look like brooks and chen
+    """
+    if vert_ind == 0:
+        radius = np.sqrt(cellCentres[:, 1] ** 2 + cellCentres[:, 2] ** 2)
+        ind_min = np.argwhere(cellCentres[:, 1] < 0)
+        radius[ind_min] = -1 * radius[ind_min]
+    elif vert_ind == 1:
+        radius = np.sqrt(cellCentres[:, 0] ** 2 + cellCentres[:, 2] ** 2)
+        ind_min = np.argwhere(cellCentres[:, 0] < 0)
+        radius[ind_min] = -1 * radius[ind_min]
+    elif vert_ind == 2:
+        radius = np.sqrt(cellCentres[:, 0] ** 2 + cellCentres[:, 1] ** 2)
+        ind_min = np.argwhere(cellCentres[:, 0] < 0)
+        radius[ind_min] = -1 * radius[ind_min]
+    else:
+        raise ValueError(f"vertical index must be 0, 1 or 2, got {vert_ind}")
+
+    return radius
+
+
+def get_heights_ind(
+    heights: list[float],
+    cellCentres: np.ndarray,
+    vert_ind: int,
+    tolerance=None,
+) -> list[np.ndarray]:
+    """
+    get the cell center indexes that correspond to the height you care about
+    """
+
+    if tolerance is None:
+        A = np.unique(cellCentres[:, vert_ind])
+        n_vert = len(A)
+        amp_vert = A.max() - A.min()
+        tolerance = 2 * amp_vert / (n_vert)
+        logger.warning(
+            f"Tolerance for conditional height not set, assuming {tolerance:.2g}"
+        )
+
+    ind_heights = {}
+    for height in heights:
+        if height < A.min() or height > A.max():
+            raise ValueError(
+                f"Height {height} is outside the mesh bounds [{A.min()}, {A.max()}]"
+            )
+
+        ind_heights[height2str(height)] = np.argwhere(
+            abs(cellCentres[:, vert_ind] - height) <= tolerance
+        )
+        n_cells_height = len(ind_heights[height2str(height)])
+        logger.debug(
+            f"found {n_cells_height} cells around height {height2str(height)}"
+        )
+        if n_cells_height == 0:
+            raise ValueError(
+                f"No cell found for height {height2str(height)}, increase tolerance or check if height {height2str(height)} is valid"
+            )
+
+    return ind_heights
+
+
+def radial_mean(
+    caseFolder=None,
+    heights: list[float] | None = None,
+    field_names: list[str] = ["CO2.liquid"],
+    vert_ind: int | None = 1,
+    window_ave=1,
+    n_bins=32,
+)->dict:
+    """
+    Compute radial conditional average
+    """
+    if caseFolder is None:
+        caseFolder = os.path.join(
+            Path(__file__).parent,
+            "..",
+            "bird",
+            "postprocess",
+            "data_conditional_mean",
+        )
+
+    logger.info(f"Case : {caseFolder}")
+
+    # Get all time folders
+    time_float_sorted, time_str_sorted = getCaseTimes(caseFolder)
+
+    # Get cell centers
+    mesh_time_str = getMeshTime(caseFolder)
+    cellCentres = readMesh(
+        os.path.join(caseFolder, f"meshCellCentres_{mesh_time_str}.obj")
+    )
+    nCells = len(cellCentres)
+    window_ave = min(window_ave, len(time_str_sorted))
+
+    # Get the cell indices that correspond to the height you care about
+    if heights is not None:
+        if vert_ind is None:
+            msg = "Assuming vertical direction is y"
+            msg += f"\nIf that is not the case, change the parameter vert_ind (currently {vert_ind})"
+            logger.warning(msg)
+            vert_ind = 1
+        ind_heights = get_heights_ind(
+            heights=heights,
+            cellCentres=cellCentres,
+            vert_ind=vert_ind,
+            tolerance=None,
+        )
+
+    # Create a radius field (to condition against)
+    radius = compute_radius_field(cellCentres, vert_ind)
+
+    # Setup the structure in which results are saved
+    fields_conds = {}
+    fields_conds_tmp = {}
+    for field_name in field_names:
+        fields_conds[field_name] = {}
+        if heights is not None:
+            radius_axis = {}
+            for height in heights:
+                fields_conds[field_name][height2str(height)] = {}
+
+    # loop over the time for which window averaging is performed
+    for i_ave in range(window_ave):
+        time_folder = time_str_sorted[-i_ave - 1]
+        logger.info(f"\tReading Time : {time_folder}")
+
+        # Get the filename of files that need to be read
+        field_file = []
+        for field_name in field_names:
+            field_file.append(
+                os.path.join(caseFolder, time_folder, field_name)
+            )
+
+        # Read
+        for filename, field_name in zip(field_file, field_names):
+            val_dict = {}
+            field_tmp = readOFScal(filename, nCells)["field"]
+
+            if heights is None:
+                radius_axis, fields_conds_tmp = conditional_average(
+                    radius, field_tmp, nbins=n_bins
+                )
+            else:
+                # Filter fields so we only look at the heights you care about
+                for height in heights:
+                    inds = ind_heights[height2str(height)]
+                    (
+                        radius_axis[height2str(height)],
+                        fields_conds_tmp[height2str(height)],
+                    ) = conditional_average(
+                        radius[inds], field_tmp[inds], nbins=n_bins
+                    )
+
+            # Do window averaging
+            if i_ave == 0:
+                if heights is None:
+                    fields_conds[field_name]["val"] = (
+                        fields_conds_tmp / window_ave
+                    )
+                    fields_cond[sfield_name]["radius"] = radius_axis
+                else:
+                    for height in heights:
+                        fields_conds[field_name][height2str(height)]["val"] = (
+                            fields_conds_tmp[height2str(height)] / window_ave
+                        )
+                        fields_conds[field_name][height2str(height)][
+                            "radius"
+                        ] = radius_axis[height2str(height)]
+
+            else:
+                if heights is None:
+                    fields_conds[field_name]["val"] += (
+                        fields_conds_tmp / window_ave
+                    )
+                else:
+                    for height in heights:
+                        fields_conds[field_name][height2str(height)][
+                            "val"
+                        ] += (
+                            fields_conds_tmp[height2str(height)] / window_ave
+                        )
+
+    return fields_conds
+
+
+if __name__ == "__main__":
+
+    # compute
+    fields_cond = radial_mean(
+        caseFolder=None,
+        heights=[5.2, 6.2, 6.3],
+        field_names=["CO2.liquid", "alpha.gas"],
+        vert_ind=1,
+        window_ave=2,
+        n_bins=32,
+    )
+
+    from prettyPlot.plotting import plt, pretty_labels
+    # plot
+    fig, axs = plt.subplots(nrows=2, ncols=3, figsize=(8, 8))
+    height_name = height2str(5.2)
+    ax = axs[0, 0]
+    ax.plot(
+        fields_cond["CO2.liquid"][height_name]["radius"],
+        fields_cond["CO2.liquid"][height_name]["val"],
+        color="k",
+        linewidth=3,
+    )
+    pretty_labels(
+        "radius[m]",
+        r"$y_{CO2, liq}$",
+        title=f"z={height_name}m",
+        fontname="Times",
+        fontsize=16,
+        ax=ax,
+        grid=False,
+    )
+
+    height_name = height2str(6.2)
+    ax = axs[0, 1]
+    ax.plot(
+        fields_cond["CO2.liquid"][height_name]["radius"],
+        fields_cond["CO2.liquid"][height_name]["val"],
+        color="k",
+        linewidth=3,
+    )
+    pretty_labels(
+        "radius[m]",
+        r"$y_{CO2, liq}$",
+        title=f"z={height_name}m",
+        fontname="Times",
+        fontsize=16,
+        ax=ax,
+        grid=False,
+    )
+
+    height_name = height2str(6.3)
+    ax = axs[0, 2]
+    ax.plot(
+        fields_cond["CO2.liquid"][height_name]["radius"],
+        fields_cond["CO2.liquid"][height_name]["val"],
+        color="k",
+        linewidth=3,
+    )
+    pretty_labels(
+        "radius[m]",
+        r"$y_{CO2, liq}$",
+        title=f"z={height_name}m",
+        fontname="Times",
+        fontsize=16,
+        ax=ax,
+        grid=False,
+    )
+
+    height_name = height2str(5.2)
+    ax = axs[1, 0]
+    ax.plot(
+        fields_cond["alpha.gas"][height_name]["radius"],
+        fields_cond["alpha.gas"][height_name]["val"],
+        color="k",
+        linewidth=3,
+    )
+    pretty_labels(
+        "radius[m]",
+        r"$\alpha_{gas}$",
+        title=f"z={height_name}m",
+        fontname="Times",
+        fontsize=16,
+        ax=ax,
+        grid=False,
+    )
+
+    height_name = height2str(6.2)
+    ax = axs[1, 1]
+    ax.plot(
+        fields_cond["alpha.gas"][height_name]["radius"],
+        fields_cond["alpha.gas"][height_name]["val"],
+        color="k",
+        linewidth=3,
+    )
+    pretty_labels(
+        "radius[m]",
+        r"$\alpha_{gas}$",
+        title=f"z={height_name}m",
+        fontname="Times",
+        fontsize=16,
+        ax=ax,
+        grid=False,
+    )
+
+    height_name = height2str(6.3)
+    ax = axs[1, 2]
+    ax.plot(
+        fields_cond["alpha.gas"][height_name]["radius"],
+        fields_cond["alpha.gas"][height_name]["val"],
+        color="k",
+        linewidth=3,
+    )
+    pretty_labels(
+        "radius[m]",
+        r"$\alpha_{gas}$",
+        title=f"z={height_name}m",
+        fontname="Times",
+        fontsize=16,
+        ax=ax,
+        grid=False,
+    )
+
+    plt.show()
