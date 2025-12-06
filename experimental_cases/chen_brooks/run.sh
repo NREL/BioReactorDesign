6a5bba65f2de300e7fbea767851e2f471ac584f8
Malik Hassanaly <mhassana@x1003c2s0b0n0.hsn.cm.kestrel.hpc.nrel.gov>
2025-12-05 17:44:32 -0700

    chen_brooks geom

---
+++ experimental_cases/chen_brooks/run.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Clean case
+./Allclean
+
+set -e  # Exit on any error
+# Define what to do on error
+trap 'echo "ERROR: Something failed! Running cleanup..."; ./Allclean' ERR
+
+
+echo PRESTEP 1
+# Generate blockmeshDict
+python ../../applications/write_block_cyl_mesh.py -i system/mesh.json -t system/topology.json -o system
+
+# Generate boundary stl
+python ../../applications/write_stl_patch.py -i system/inlets_outlets.json
+
+# Generate species thermo properties
+python ../../applications/write_species_thermo_prop.py -cf .
+
+
+echo PRESTEP 2
+# Mesh gen
+blockMesh -dict system/blockMeshDict
+transformPoints "rotate=((0 0 1) (0 1 0))"
+transformPoints "scale=(0.001 0.001 0.001)"
+snappyHexMesh -overwrite
+
+# Inlet BC
+surfaceToPatch -tol 1e-4 inlets.stl
+export newmeshdir=$(foamListTimes -latestTime)
+rm -rf constant/polyMesh/
+cp -r $newmeshdir/polyMesh ./constant
+rm -rf $newmeshdir
+cp constant/polyMesh/boundary /tmp
+sed -i -e 's/inlets\.stl/inlet/g' /tmp/boundary
+cat /tmp/boundary > constant/polyMesh/boundary
+
+# setup IC
+cp -r 0.orig 0
+setFields
+
+# Setup mass flow rate
+# Get inlet area
+postProcess -func 'patchIntegrate(patch="inlet", field="alpha.gas")'
+postProcess -func writeCellVolumes
+writeMeshObj
+
+echo PRESTEP 3
+python writeGlobalVars.py
+cp constant/phaseProperties_pbe constant/phaseProperties
+touch test.foam
+
+echo RUN
+birdmultiphaseEulerFoam
