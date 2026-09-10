824a222c02b577bc511b745168dbc457c899c27e
Malik <malik.hassanaly@gmail.com>
2026-09-10 14:38:48 -0600

    Add mesh-agnostic loop velocity post-processing

--- bird/postprocess/post_quantities/__init__.py
+++ bird/postprocess/post_quantities/__init__.py
@@ -6,16 +6,26 @@ from ._cell_filter import (
     _weighted_average,
 )
 from .kla import compute_fitted_kla, compute_instantaneous_kla
+from .loop_velocity import (
+    build_loop_direction_field,
+    build_loop_direction_field_from_path,
+    compute_loop_velocity,
+    propose_loop_boxes_block_rect,
+)
 from .phase import compute_ave_bubble_diam, compute_gas_holdup
 from .species import compute_ave_conc_liq, compute_ave_y_liq
 from .superficial_velocity import compute_superficial_gas_velocity
 
 __all__ = [
+    "build_loop_direction_field",
+    "build_loop_direction_field_from_path",
     "compute_ave_bubble_diam",
     "compute_ave_conc_liq",
     "compute_ave_y_liq",
     "compute_fitted_kla",
     "compute_gas_holdup",
     "compute_instantaneous_kla",
+    "compute_loop_velocity",
     "compute_superficial_gas_velocity",
+    "propose_loop_boxes_block_rect",
 ]
