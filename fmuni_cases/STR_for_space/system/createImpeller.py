69e5db8e400d5bd27fa894ddbfce7c832c04af6a
fmuni <federico.municchi@gmail.com>
2026-09-10 00:42:14 -0600

    Update to rotating STR

--- fmuni_cases/STR_for_space/system/createImpeller.py
+++ fmuni_cases/STR_for_space/system/createImpeller.py
@@ -30,9 +30,11 @@ shaft_R   =   0.15
 disk_h =      0.1
 disk_R =      0.5
 blade_H =     0.5
-blade_L =     0.8
+blade_L =     0.6
 blade_t =     0.05
 blade_attack = 0.3
+
+impeller_sep = 1
 
 disk_max = shaft_min + disk_h
 disk_mid = (disk_max + shaft_min)/2.0
@@ -48,8 +50,10 @@ blade_z0 = disk_mid - blade_H/2.0
 blade_z1 = disk_mid + blade_H/2.0
 
 rzone_min = blade_z0 - blade_H/4.0
-rzone_max = blade_z1 + blade_H/4.0
+rzone_max = blade_z1 + blade_H/4.0 + impeller_sep
 rzone_R = blade_x1 + blade_L/4.0
+
+
 
 with open("impeller", "w", encoding="utf-8") as file:
     file.write( "impeller_rotatingZone\n")
@@ -103,6 +107,27 @@ for i in range(6):
     e2x = -math.sin(angle)
     e2y = math.cos(angle)
     addSurface("impeller_blade", i, 0, 0, 0, e1x, e1y, 0, e2x, e2y, 0, 0, 0, 1 )
+
+with open("impeller", "a", encoding="utf-8") as file:
+    file.write( "}\n")
+
+## Impeller 2    
+with open("impeller", "a", encoding="utf-8") as file:
+    file.write( "impeller2\n")
+    file.write( "{\n")
+    file.write( "   type collection;\n")
+    file.write( "   mergeSubRegions true;\n")
+    
+
+addSurface("impeller_disk", 0, 0, 0, impeller_sep, 1, 0, 0, 0, 1, 0, 0, 0, 1 )
+
+for i in range(6):
+    angle = i * 2 * math.pi / 6.0
+    e1x = math.cos(angle)
+    e1y = math.sin(angle)
+    e2x = -math.sin(angle)
+    e2y = math.cos(angle)
+    addSurface("impeller_blade", i, 0, 0, impeller_sep, e1x, e1y, 0, e2x, e2y, 0, 0, 0, 1 )
 
 with open("impeller", "a", encoding="utf-8") as file:
     file.write( "}\n")
